90ae3620a0a25f4ad39ba3c0d5c558615b0a4ca2
koniki <nk5011997@gmail.com>
2017-02-07 22:16:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,8 @@
 console.log('Loaded!');
 var img = document.getElementById('img');
-var margin=0;
-/*function move()
-{
-     margin=margin+10;
-    img.style.margin= margin+'px';
-    
-  
-    
-    
-}*/
 img.onclick=function()
 {
-   //var interval = setInterval(move,10);
+   
     img.style.marginLeft('100px');
     
     
