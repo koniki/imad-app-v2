318cae21878d1cf54c3e06e74196d4ab0746462a
koniki <nk5011997@gmail.com>
2017-02-10 22:26:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,34 @@ var click= document.getElementById('nik');
 var counter=0;
 click.onclick=function()
 {
-      counter=counter+1;
+// make a request to webpage
+var request =new XMLHttprequest();
+request.onreadystatechange=function()
+{
+  if(request.readyState===XMLHttpResponse.DONE)
+  {
+      if(request.status===200)
+      {
+          var counter= request.responsetext;
+           
  document.getElementById('span').textContent=counter;
  
 // or apply this code
 // span.innerHTML=counter.toString();
+          
+      }
+      
+      
+  }
+    
+};
+
+    
+    
+    
+    
+     
+
+
     
 };
