37b492030793b14846a94474510253a6cb424ad5
koniki <nk5011997@gmail.com>
2017-02-10 22:06:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,7 @@ click.onclick=function()
       counter=counter+1;
  document.getElementById('span').textContent=counter;
  
-    
+// or apply this code
+// span.innerHTML=counter.toString();
     
 };
